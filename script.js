c3d32a347bb33b6525631029f078a810c1212239
IgorSousaa <igoralvesnasc2@gmail.com>
2022-06-21 00:20:33 -0300

    Atividade de Funções

---
+++ script.js
@@ -0,0 +1,31 @@
+function boasVindas (){
+    console.log("Seja bem vindo!")
+}
+
+boasVindas()
+
+// ========================================================================== //
+
+function eu (nome){
+    console.log("Seja Bem-vindo", nome);
+}
+
+eu("Igor")
+
+// ========================================================================== //
+
+function itens (nome, idade, estiloMusical){
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e meu estilo musical que eu mais gosto é ${estiloMusical}`)
+}
+
+itens("Igor", 18, "Trap e Funk")
+
+// ========================================================================== //
+
+function entretenimento (filme, musica){
+    console.log(`Meu filme favorito é ${filme}, e minha música favorita é ${musica}`)
+}
+
+entretenimento("Arremesando Alto", "O menino que virou Deus")
+
+// ========================================================================== //
